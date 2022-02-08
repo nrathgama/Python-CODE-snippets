4e967b4c18b78702813f6dd632039d4b30388922
NILU RA <70004867+nrathgama@users.noreply.github.com>
2022-02-07 23:38:50 -0600

    Update modt.py

--- modt.py
+++ modt.py
@@ -3,14 +3,14 @@ import turtle
 t = turtle.Turtle()
 t.width(5)
 t.color ("red")
-t.penup()
-t.back(100)
-t.pendown
+#t.penup()
+#t.back(100)
+#t.pendown
 
 for step in range(21):
     t.forward(10)
 
-if step%3 == 0:
+if step%2 == 0:
     t.left(90)
 else: 
     t.right(90)
